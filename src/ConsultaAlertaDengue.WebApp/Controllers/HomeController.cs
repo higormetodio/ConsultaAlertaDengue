ac1f26532d6062cbf9c6bed179573f9cfa6a0368
Higor Metodio <higor.metodio@outlook.com>
2025-04-07 13:33:55 -0300

    Criando o projeto Dados Dengue Web APP

---
+++ src/ConsultaAlertaDengue.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using ConsultaAlertaDengue.Application.UseCases.GetDadosDengueUseCase;
+using ConsultaAlertaDengue.Application.UseCases.RegisterDadosDengueUseCase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsultaAlertaDengue.WebApp.Controllers;
+
+public class HomeController : Controller
+{    
+    private readonly IGetDadosDengueUseCase _getDadosDengueUseCase;
+    private readonly IRegisterDadosDengueUseCase _registerDadosDengueUseCase;
+
+    public HomeController(IGetDadosDengueUseCase getDadosDengueUseCase, IRegisterDadosDengueUseCase registerDadosDengueUseCase)
+    {
+        _getDadosDengueUseCase = getDadosDengueUseCase;
+        _registerDadosDengueUseCase = registerDadosDengueUseCase;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var dadosDengue = await _getDadosDengueUseCase.ExecuteAsync();
+        
+        return View(dadosDengue);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GerarDadosAlertaDengue()
+    {
+        var result = await _registerDadosDengueUseCase.ExecuteAsync();
+
+        return View("Index", result);
+    }
+}
