67cc85deed4fc04d1cdd5d52851f7ecc2eb658a2
Higor Metodio <higor.metodio@outlook.com>
2025-04-07 13:36:57 -0300

    Criando o projeto Consulta Alerta Dengue API

---
+++ src/ConsultaAlertaDengue.API/Controllers/DadosDengueController.cs
@@ -0,0 +1,34 @@
+﻿using ConsultaAlertaDengue.Application.Models;
+using ConsultaAlertaDengue.Application.UseCases.GetDadosDengueBySeAno;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsultaAlertaDengue.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class DadosDengueController : ControllerBase
+{
+    private readonly IGetDadosDengueBySeAnoUseCase _getDadosDengueBySeAnoUseCase;
+
+    public DadosDengueController(IGetDadosDengueBySeAnoUseCase getDadosDengueBySeAnoUseCase)
+    {
+        _getDadosDengueBySeAnoUseCase = getDadosDengueBySeAnoUseCase;
+    }
+
+    [HttpGet()]
+    [ProducesResponseType(typeof(ResultViewModel<DadosDengueViewModel>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultViewModel<DadosDengueViewModel>), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetDadosDengueBySeAno(
+        [FromQuery] int semana,
+        [FromQuery] int ano)
+    {
+        var result = await _getDadosDengueBySeAnoUseCase.ExecuteAsync(semana, ano);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Data);
+        }
+
+        return NotFound(result);
+    }
+}
