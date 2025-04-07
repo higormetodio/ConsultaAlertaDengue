87f701b2fb8d7e3fc0b1efa4545a2303de53170d
Higor Metodio <higor.metodio@outlook.com>
2025-04-07 13:39:06 -0300

    Testes de Unidade para o Projeto Consulta Alerta Dengue WebAPP

---
+++ tests/WebApp.Test/Controllers/GerarDadosDengueTest.cs
@@ -0,0 +1,36 @@
+﻿using CommonTestUtilities.Services;
+using ConsultaAlertaDengue.Application.Models;
+using ConsultaAlertaDengue.Application.UseCases.RegisterDadosDengueUseCase;
+using ConsultaAlertaDengue.WebApp.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Test.Controllers;
+
+public class GerarDadosDengueTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var (success, message) = (true, "Dados atualizados com sucesso.");
+
+        var model = new ResultViewModel<string>(message);
+        
+        var useCase = CreateUseCase(success, message);
+
+        var controller = new HomeController(null, useCase);
+
+        var result = await controller.GerarDadosAlertaDengue();
+
+        result.Should().BeOfType<ViewResult>().Which.ViewName.Should().Be("Index");
+    }
+
+    private RegisterDadosDengueUseCase CreateUseCase(bool success, string message)
+    {
+        var service = new AlertaDengueServiceBuilder();
+
+        service.AtualizaDadosDengue(success, message);
+
+        return new RegisterDadosDengueUseCase(service.Builder());
+    }
+}
---
+++ tests/WebApp.Test/Controllers/IndexTest.cs
@@ -0,0 +1,37 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using ConsultaAlertaDengue.Application.UseCases.GetDadosDengueUseCase;
+using ConsultaAlertaDengue.Domain.Entities;
+using ConsultaAlertaDengue.WebApp.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Test.Controllers;
+
+public class IndexTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var dadosDengue = ListDadosDengueBuilder.Builder();
+
+        var useCase = CreateUseCase(dadosDengue);
+
+        var controller = new HomeController(useCase, null);
+
+        var result = await controller.Index();
+
+        result.Should().BeOfType<ViewResult>();
+    }
+
+    private GetDadosDengueUseCase CreateUseCase(List<DadosDengue> dadosDengue)
+    {
+        var repository = new DadosDengueReadOnlyRespositoryBuilder();
+
+        repository.GetDadosDengue(dadosDengue);
+
+        return new GetDadosDengueUseCase(repository.Builder());
+    }
+}
+
+
